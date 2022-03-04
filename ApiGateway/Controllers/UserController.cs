39cd5f2207709e66e80bdb0ed25be80943bbd6e1
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-03-04 23:27:29 +0700

    Thêm controller user

---
+++ ApiGateway/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using MongoDbServices;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserModel user)
+        {
+            if (user is null) return BadRequest();
+            await _userService.AddUser(user);
+            //return CreatedAtAction(nameof(Get), new { username = user.UserName }, user);
+            return Ok(new
+            {
+                Code = 1,
+                Message = "Successful",
+                Data = user
+            });
+        }
+        [HttpGet]
+        public async Task<ActionResult<UserModel>> Get(string username, string passwd)
+        {
+            var user = await _userService.GetUser(username, passwd);
+            if (user is null) return BadRequest();
+            return Ok(user);
+        }
+    }
+}
